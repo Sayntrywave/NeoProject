9c81a4802182e3d70b4aed5d2ded76dd671c8b41
Никитос <Sayntrywave@yandex.ru>
2023-05-15 20:33:44 +0300

    added input validation

--- src/main/java/ru/korotkov/neo/controllers/VacationController.java
+++ src/main/java/ru/korotkov/neo/controllers/VacationController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import ru.korotkov.neo.models.VacationRequest;
 
 
@@ -16,19 +17,14 @@ public class VacationController {
     }
 
     @GetMapping("/calculate")
-    public String calculateVacation(@Valid VacationRequest vacationRequest, BindingResult bindingResult, Model model) {
-
+    public String calculateVacation(@Valid @ModelAttribute("vacationRequest") VacationRequest vacationRequest, BindingResult bindingResult, Model model) {
         if (!bindingResult.hasErrors()) {
             long vacationPay = vacationRequest.calculateVacationPay();
-
-            model.addAttribute("salary", vacationRequest.salary());
-            model.addAttribute("vacationDays", vacationRequest.vacationDays());
             model.addAttribute("vacationPay", vacationPay);
         }
-
-
         return "calculator";
     }
 }
+
 
 
--- src/main/java/ru/korotkov/neo/models/VacationRequest.java
+++ src/main/java/ru/korotkov/neo/models/VacationRequest.java
@@ -5,7 +5,7 @@ import org.hibernate.validator.constraints.Range;
 
 public record VacationRequest(
         @NotNull @Range(min = 0, max = 1_000_000, message = "Salary should be between 0 and 1m") Double salary,
-        @NotNull @Range(min = 1, max = 365, message = "amount of vacation days should be between 1 and 365") Integer vacationDays) {
+        @NotNull @Range(min = 0, max = 365, message = "amount of vacation days should be between 0 and 365") Integer vacationDays) {
     public long calculateVacationPay() {
         return Math.round(salary / 365 * vacationDays);
     }
